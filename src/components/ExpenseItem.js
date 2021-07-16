ea697a1bd348384fb2238d111516e8589843124f
Sharvin Pharande <52949072+Sharvinp@users.noreply.github.com>
2021-07-16 22:20:11 +0530

    adding components

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <h2>Title</h2>
+        <div>Title</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
